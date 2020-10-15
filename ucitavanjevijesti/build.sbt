023cca1d58665fcf138e8b5fd54fb8d3e9d7a7c9
Vedad Sehic <vedad071@gmail.com>
2020-10-15 21:28:12 +0200

    scala_based_project
    With all associated files.

---
+++ ucitavanjevijesti/build.sbt
@@ -0,0 +1,5 @@
+name := "ucitavanjevijesti"
+
+version := "0.1"
+
+scalaVersion := "2.13.3"
---
+++ ucitavanjevijesti/src/main/scala/FinancialNewsLoading/SequentialLoading.scala
@@ -0,0 +1,44 @@
+package FinancialNewsLoading
+
+import java.text.SimpleDateFormat
+import java.util.Date
+
+import scala.io.Source
+
+object SequentialLoading {
+
+  case class FinancialNews(id: Int, symbol: String, title: String, publicationDate: Date, providerUrl: String)
+
+    def loadFile(fileName: String): Int = {
+      Thread.sleep(5000)
+
+      val source = Source.fromFile(fileName)
+      val newsSequence: Seq[String] = source.getLines().toSeq.tail
+
+      var fileContent: List[String] = List()
+      val dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+
+      val news: Seq[FinancialNews] = newsSequence.tail.map { seqStr =>
+        val parts: Seq[String] = seqStr.split("\\|")
+
+          FinancialNews(parts(0).toInt, parts(1).trim, parts(2).trim, dateFormat.parse(parts(3)), parts(4).trim)
+        }
+
+      for (pieceOfNews <- news) {
+        fileContent = fileContent :+ (pieceOfNews.id + "\t" + pieceOfNews.symbol + "\t" + pieceOfNews.title + "\t" + pieceOfNews.publicationDate + "\t" + pieceOfNews.providerUrl)
+      }
+      fileContent.size
+    }
+
+    def main(args: Array[String]): Unit = {
+      val starter = System.currentTimeMillis()
+      var i = 1
+      while(i < 11) {
+        println("Loaded news number -> " + loadFile(s"project\\Zadaca2File${i}.txt") + s" (Zadaca2File${i}.txt)")
+        Thread.sleep(5000)
+        i += 1
+      }
+      println(s"Loaded files number: ${i - 1}. Total time duration: ${(System.currentTimeMillis() - starter)/1000} seconds.")
+    }
+
+}
