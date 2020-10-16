594fd6b87add22b9816d3fb1647e5c8e144d3df1
VedadSe <71317602+VedadSe@users.noreply.github.com>
2020-10-17 00:30:18 +0200

    Introduced Try-Catch Block
    case ex: Throwable => 0

--- ucitavanjevijesti/src/main/scala/FinancialNewsLoading/SequentialLoading.scala
+++ ucitavanjevijesti/src/main/scala/FinancialNewsLoading/SequentialLoading.scala
@@ -10,22 +10,27 @@ object SequentialLoading {
   case class FinancialNews(id: Int, symbol: String, title: String, publicationDate: Date, providerUrl: String)
 
     def loadFile(fileName: String): Int = {
-      val source = Source.fromFile(fileName)
-      val newsSequence: Seq[String] = source.getLines().toSeq
+      try {
+        val source = Source.fromFile(fileName)
+        val newsSequence: Seq[String] = source.getLines().toSeq
 
-      var fileContent: List[String] = List()
-      val dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+        var fileContent: List[String] = List()
+        val dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
 
-      val news: Seq[FinancialNews] = newsSequence.takeRight(newsSequence.size - 2).map { seqStr =>
-        val parts: Seq[String] = seqStr.split("\\|")
+        val news: Seq[FinancialNews] = newsSequence.takeRight(newsSequence.size - 2).map { seqStr =>
+          val parts: Seq[String] = seqStr.split("\\|")
 
-          FinancialNews(parts(0).toInt, parts(1).trim, parts(2).trim, dateFormat.parse(parts(3)), parts(4).trim)
+            FinancialNews(parts(0).toInt, parts(1).trim, parts(2).trim, dateFormat.parse(parts(3)), parts(4).trim)
         }
 
-      for (pieceOfNews <- news) {
-        fileContent = fileContent :+ (pieceOfNews.id + "\t" + pieceOfNews.symbol + "\t" + pieceOfNews.title + "\t" + pieceOfNews.publicationDate + "\t" + pieceOfNews.providerUrl)
+        for (pieceOfNews <- news) {
+          fileContent = fileContent :+ (pieceOfNews.id + "\t" + pieceOfNews.symbol + "\t" + pieceOfNews.title + "\t" + pieceOfNews.publicationDate + "\t" + pieceOfNews.providerUrl)
+        }
+        fileContent.size
       }
-      fileContent.size
+      catch {
+        case ex: Throwable => 0
+      }
     }
 
     def main(args: Array[String]): Unit = {
